d389573d5c4f3e87a5a2acd830dae2b55509f990
Warszawski19 <michal.tl.krakow@gmail.com>
2022-07-10 17:11:35 +0200

    Scroll top button

--- elements/footer_bottom.php
+++ elements/footer_bottom.php
@@ -17,6 +17,12 @@
     </div>
 </aside>
 
+<button type="button" class="btn btn-secondary btn-floating btn-lg" style="position: fixed; z-index: 95; bottom: 20px;right: 20px; display: none; border: 1px solid #000;" id="btn-back-to-top">
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-up" viewBox="0 0 16 16">
+        <path fill-rule="evenodd" d="M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5z"/>
+    </svg>
+</button>
+
 <?php $this->inc('elements/scripts.php');?>
 
 </body>
--- elements/scripts.php
+++ elements/scripts.php
@@ -215,3 +215,29 @@
         change_mode("dark")
     }
 </script>
+
+<script>
+    // SCROLL TOP BUTTON
+    let top_button = document.getElementById("btn-back-to-top");
+
+    window.onscroll = function () {
+        scrollFunction();
+    };
+
+    function scrollFunction() {
+        if (
+            document.body.scrollTop > 20 ||
+            document.documentElement.scrollTop > 20
+        ) {
+            top_button.style.display = "block";
+        } else {
+            top_button.style.display = "none";
+        }
+    }
+    top_button.addEventListener("click", backToTop);
+
+    function backToTop() {
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+    }
+</script>
